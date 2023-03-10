a0cff1895345cf1eacad448a7c86baf41d1e9969
Hugand <hugoasgomes@hotmail.com>
2023-03-10 16:55:14 +0000

    Create tpc3 base

---
+++ TPC3/decision_tree.py
@@ -0,0 +1,42 @@
+from dataset import Dataset
+
+class DecisionTree:
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
+
+    # Escolha de atributos
+    def __entropy(self):
+        pass
+
+    def __gini(self):
+        pass
+
+    def __gain_ratio(self):
+        pass
+
+    # Resolução de conflitos
+    def __prune(self):
+        pass
+
+    def __majority_voting(self):
+        pass
+
+    # Pre-prunning
+    def __size_prepruning(self):
+        pass
+
+    def __maximum_depth_prepruning(self):
+        pass
+
+    # Post-prunning
+    def __pessimistic_error_pruning(self):
+        pass
+
+    def __reduced_error_pruning(self):
+        pass
