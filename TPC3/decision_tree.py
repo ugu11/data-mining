c268b2274a74550545ebeca5580835e66f288ffb
Anaa10 <a93284@alunos.uminho.pt>
2023-03-17 02:32:56 +0000

    funções fit e predict acabadas

--- TPC3/decision_tree.py
+++ TPC3/decision_tree.py
@@ -32,17 +32,17 @@ class DecisionTrees:
 
         self.tree = None
         
-    # def predict(self, X):
-    #     return np.array([self.traverse_tree(x, self.tree) for x in X])
+    def my_predict(self, X):
+        return np.array([self.traverse_tree(x, self.tree) for x in X])
 
-    # def traverse_tree(self, x, node):
-    #     if node.leaf:
-    #         return node.value
+    def traverse_tree(self, x, node):
+        if node.leaf:
+            return node.value
         
-    #     if x[node.feature] < node.threshold:
-    #         return self.traverse_tree(x, node.left)
-    #     else:
-    #         return self.traverse_tree(x, node.right)
+        if x[node.feature] < node.threshold:
+            return self.traverse_tree(x, node.left)
+        else:
+            return self.traverse_tree(x, node.right)
 
     def fit(self, X, y):
         self.tree = self.build_tree(X,y,0)
@@ -50,26 +50,32 @@ class DecisionTrees:
     def build_tree(self, X, y, depth=0):
         n_samples, n_features = X.shape
         n_labels = len(np.unique(y))
-        
-        # Check termination conditions
-        # if depth == self.max_depth or n_samples < self.min_samples_split or n_labels == 1:
-        #     value = self.most_common(y)
-        #     leaf = Node(leaf = True, value = value)
-        #     return leaf
+
+
+        if depth == self.max_depth or n_samples < self.min_samples_split or n_labels == 1:
+            if y is None:
+                leaf = Node(leaf = True)
+            else:
+                value = self.most_common(y)
+                leaf = Node(leaf = True, value = value)
+            return leaf
         
         # Find best split
         best_feature, best_threshold = self.find_best_split(X, y, n_features)
 
-        # Check termination condition
-        # if best_feature is None:
-        #     value = self.most_common(y)
-        #     leaf = Node(leaf = True, value = value)
-        #     return leaf
+        if best_feature is None:
+            if y is None:
+                leaf = Node(leaf = True)
+            else:
+                value = self.most_common(y)
+                leaf = Node(leaf = True, value = value)
+            return leaf
         
         node = Node(feature = best_feature, threshold = best_threshold, leaf = False)
+
         # Split data and recursively build subtrees
-        left_indices = X[:, best_feature] < best_threshold
-        right_indices = X[:, best_feature] >= best_threshold
+        left_indices = X[:, best_feature] <= best_threshold
+        right_indices = X[:, best_feature] > best_threshold
         
         node.left = self.build_tree(X[left_indices], y[left_indices], depth+1)
         node.right = self.build_tree(X[right_indices], y[right_indices], depth+1)
@@ -80,7 +86,7 @@ class DecisionTrees:
         best_gain = -1
         best_feature = None
         best_threshold = None
-        
+
         for feature_idx in range(n_features):
             feature_values = X[:, feature_idx]
             thresholds = np.unique(feature_values)
@@ -103,8 +109,8 @@ class DecisionTrees:
 
                 information_gain_before = self.apply_criterion(self.criterion,y,feature_values)
                 information_gain_left = self.apply_criterion(self.criterion,lefty,feature_values) * (len(lefty)/len(y))
-                information_gain_rigth = self.apply_criterion(self.criterion,righty,feature_values) * (len(righty)/len(y))
-                information_gain = information_gain_before - (information_gain_left + information_gain_left)
+                information_gain_right = self.apply_criterion(self.criterion,righty,feature_values) * (len(righty)/len(y))
+                information_gain = information_gain_before - (information_gain_left + information_gain_right)
                 
                 if information_gain > best_gain:
                     best_gain = information_gain
@@ -133,9 +139,26 @@ class DecisionTrees:
         """
         Return the most common attribute value in y
         """
-        _, counts = np.unique(y, return_counts=True)
-        value = np.argmax(counts)
-        return value
+        # _, counts = np.unique(y, return_counts=True)
+        # print(counts)
+        # if counts is None:
+        #     value = 0
+        # else:
+        #     value = np.argmax(counts)
+        # return value
+        values = {}
+        for value in y:
+            if value in values:
+                values[value] += 1
+            else:
+                values[value] = 1
+        max_values = 0
+        most_common = None
+        for value, count in values.items():
+            if count > max_values:
+                max_values = count
+                most_common = value
+        return most_common
     
 
     # Escolha de atributos
@@ -191,18 +214,22 @@ class DecisionTrees:
 if __name__ == '__main__':
     from dataset import Dataset
     from decision_tree import DecisionTrees
+    from sklearn.tree import DecisionTreeClassifier
     from sklearn.model_selection import train_test_split
     from sklearn.metrics import accuracy_score
 
     data = Dataset('teste.csv',label='Outlook')
-    # X_train, X_test, y_train, y_test = train_test_split(data.X, data.y, test_size=0.2, random_state=1234)
+    X_train, X_test, y_train, y_test = train_test_split(data.X, data.y, test_size=0.2, random_state=1234)
     
-    # clf = DecisionTrees(max_depth=10, criterion='entropy')
-    # clf.fit(X_train, y_train)
-    # y_pred = clf.predict(X_test)
-    # accuracy = accuracy_score(y_test, y_pred)
-    # print(accuracy)
-    tree = DecisionTrees()
-    entropy = tree.find_best_split(data.X,data.y,data.X.shape[1])
-    # print(data.label)
-    print(entropy)
+    clf = DecisionTrees(max_depth=6,criterion='entropy')
+    clf.fit(X_train, y_train)
+    y_pred = clf.my_predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print(accuracy)
+
+    clf2 = DecisionTreeClassifier(max_depth=6)
+    clf2.fit(X_train, y_train)
+    y_pred2 = clf2.predict(X_test)
+    accuracy2 = accuracy_score(y_test, y_pred2)
+    print(accuracy)
+
