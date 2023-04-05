49bc43ba45f3228f867582e413c98acf49f58cb7
Hugand <hugoasgomes@hotmail.com>
2023-04-05 18:26:28 +0100

    Document prism class

--- TPC4/prism.py
+++ TPC4/prism.py
@@ -1,11 +1,26 @@
 import numpy as np
 from dataset import Dataset
+from typing import Tuple, Sequence, List
 
 class Prism:
     def __init__(self):
         pass
 
-    def fit(self, dataset):
+    def __repr__(self):
+        """
+        Print the infered rules
+        -------
+        tuple (n_samples, n_features)
+        """
+        return str(self.rules)
+
+    def fit(self, dataset: Dataset):
+        """
+        Fit the classifier
+        -------
+        dataset: Dataset
+            The dataset used to fit the classifier
+        """
         X, y = np.copy(dataset.X), np.copy(dataset.y)
         unique_labels = np.unique(y)
         rules = []
@@ -18,32 +33,44 @@ class Prism:
                 l_idx = y == label
                 if np.sum(l_idx) == 0:
                     break
-                subset = X[l_idx == True]
+                # subset = X[l_idx == True]
 
-                # Build first probabilities table
-                total_probs = self.__build_prob_table(dataset, X, subset)
-                max_feat, max_val = self.__get_best_feature(total_probs, dataset)
-                subset = subset[subset.T[max_feat] == max_val]
+                # # Build first probabilities table
+                # probability_table_1 = self.__build_single_probability_table(dataset, X, subset)
+                # best_feature_1pt, best_val_1pt = self.__get_best_feature(probability_table_1, dataset)
 
-                # Build second probabilities table with pairs of features
-                total_probs2 = self.__build_prob_table(dataset, X, subset, max_feat, max_val)
-                best_match_feature, best_match_val = self.__get_best_feature(total_probs2, dataset)
+                # # Get new subset
+                # subset = subset[subset.T[best_feature_1pt] == best_val_1pt]
+
+                # # Build second probabilities table with pairs of features
+                # probability_table_2 = self.__build_single_probability_table(dataset, X, subset, best_feature_1pt, best_val_1pt)
+                # best_feature_2pt, best_val_2pt = self.__get_best_feature(probability_table_2, dataset)
+
+                best_feature_1pt, best_feature_2pt = self.__get_best_features(X, l_idx, dataset)
 
                 # Create and append new rule
                 new_rule = self.__create_rule(
-                    max_feat, max_val,
-                    best_match_feature, best_match_val,
+                    best_feature_1pt[0], best_feature_1pt[1],
+                    best_feature_2pt[0], best_feature_2pt[1],
                     label, rule_size)
                 rules.append(new_rule)
 
                 # Drop rows
-                drop_idx = np.logical_and(X.T[best_match_feature] == best_match_val, X.T[max_feat] == max_val)
+                drop_idx = np.logical_and(X.T[best_feature_2pt[0]] == best_feature_2pt[1], X.T[best_feature_1pt[0]] == best_feature_1pt[1])
                 X = np.delete(X, drop_idx, axis=0)
                 y = np.delete(y, drop_idx, axis=0)
 
         self.rules = np.array(rules)
 
-    def predict(self, X):
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Predict result based on input data
+        -------
+        X: numpy.ndarray (n_samples, n_features)
+            Input data
+        -------
+        numpy.ndarray (n_samples)
+        """
         output = []
         # for x in X:
         outcomes = []
@@ -60,11 +87,59 @@ class Prism:
         most_frequent = values[counts.argmax()] 
 
         return most_frequent
-            
-            
 
+    def __get_best_features(self, X: np.ndarray, l_idx: int, dataset: Dataset) -> Tuple[Tuple[int, int], Tuple[int, int]]:
+        """
+        Get the best feautures to build a rule
+        -------
+        X: numpy.ndarray ()
+            Input data being used to build the table
+        l_idx: int
+            Indices for the subset
+        dataset: Dataset
+            Dataset object being used
+        -------
+        Tuple[Tuple[int, int], Tuple[int, int]]
+            Tuple of features. Each feature has the index of the feature in the dataset (int) and the value of the feature (int).
+        """
+        subset = X[l_idx == True]
 
-    def __build_prob_table(self, dataset, X, subset, max_feat=None, max_val=None):
+        # Build first probabilities table
+        probability_table_1 = self.__build_single_probability_table(dataset, X, subset)
+        best_feature_1pt, best_val_1pt = self.__get_best_feature(probability_table_1, dataset.feature_names)
+
+        # Get new subset
+        subset = subset[subset.T[best_feature_1pt] == best_val_1pt]
+
+        # Build second probabilities table with pairs of features
+        probability_table_2 = self.__build_single_probability_table(dataset, X, subset, best_feature_1pt, best_val_1pt)
+        best_feature_2pt, best_val_2pt = self.__get_best_feature(probability_table_2, dataset.feature_names)
+
+        return (best_feature_1pt, best_val_1pt), (best_feature_2pt, best_val_2pt)
+
+    def __build_single_probability_table(self,
+        dataset: Dataset,
+        X: np.ndarray,
+        subset: np.ndarray,
+        max_feat: int=None,
+        max_val: float=None) -> np.ndarray:
+        """
+        Build single probability table
+        -------
+        dataset: Dataset
+            Dataset object being used
+        X: numpy.ndarray ()
+            Input data being used to build the table
+        subset: numpy.ndarray
+            X subset
+        max_feat: int (Optional) 
+            Feature with the max probability from the first table built
+        max_val: float (Optional) 
+            Value of the best feature from the first table built
+        -------
+        numpy.ndarray (n_samples)
+            Probability table
+        """
         total_probs = [[], [], []]
         _X = X if max_feat == None or max_val == None else dataset.X
 
@@ -90,27 +165,57 @@ class Prism:
 
         return total_probs
     
-    def __create_rule(self, max_feature, max_val, best_match_feature, best_match_val, label, rule_size):
+    def __create_rule(self,
+        best_feature_1pt:int,
+        best_val_1pt:float,
+        best_feature_2pt:int,
+        best_val_2pt:float,
+        label:int,
+        rule_size:int) -> list:
+        """
+        Create a rule
+        -------
+        best_feature_1pt: int
+            Best feature of the first probability table
+        best_val_1pt: float
+            Value of the best feature of the first probability table
+        best_feature_2pt: int
+            Best feature of the second probability table
+        best_val_2pt: float
+            Value of the best feature of the second probability table
+        -------
+        list
+            New rule
+        """
         new_rule = [-1] * rule_size
-        new_rule[best_match_feature] = best_match_val #int(float(best_match_feat[1]))
-        new_rule[max_feature] = max_val
+        new_rule[best_feature_1pt] = best_val_1pt
+        new_rule[best_feature_2pt] = best_val_2pt
         new_rule[-1] = label
 
         return new_rule
 
-    def __get_best_feature(self, probs, dataset):
-        highest_prob_idx = np.argmax(probs, axis=1)[2]
-        feature = probs.T[highest_prob_idx]
-        feature_idx = dataset.feature_names.index(feature[0])
+    def __get_best_feature(self, probablity_table: np.ndarray, feature_names: List[str]) -> Tuple[int, int]:
+        """
+        Create a rule
+        -------
+        probability_table: numpy.ndarray
+            Probabilty table
+        dataset: numpy.ndarray
+            Probabilty table
+        -------
+        list
+            New rule
+        """
+        highest_prob_idx = np.argmax(probablity_table, axis=1)[2]
+        feature = probablity_table.T[highest_prob_idx]
+        feature_idx = feature_names.index(feature[0])
 
         return feature_idx, int(float(feature[1]))
-
-
         
 if __name__ == '__main__':
     d = Dataset('teste.csv')
     p = Prism()
     p.fit(d)
-    print(p.rules)
     pred = p.predict(np.array([2, 1, 0, 1]))
-    print(pred)
+    print("Prediction:", pred)
+    print(p)
