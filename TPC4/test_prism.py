3f981758483def626fe63351daf5c8539ff29c3d
Hugand <hugoasgomes@hotmail.com>
2023-04-05 19:27:31 +0100

    Add documentation for prism unit tests

--- TPC4/test_prism.py
+++ TPC4/test_prism.py
@@ -6,6 +6,9 @@ from prism import Prism
 
 class TestPrismClassifier(unittest.TestCase):
     def test_is_fit_working(self):
+        '''
+            Test if the Prism classifier is infering the rules properly
+        '''
         d = Dataset('teste.csv')
         p = Prism()
         p.fit(d)
@@ -21,6 +24,9 @@ class TestPrismClassifier(unittest.TestCase):
         print("[test_is_fit_working]:", 'Pass' if result else 'Failed')
 
     def test_is_prediction_correct_for_multiple_inputs(self):
+        '''
+            Test if the Prism classifier is predicting correctly for multiple inputs (2D array)
+        '''
         d = Dataset('teste.csv')
         p = Prism()
         p.fit(d)
@@ -37,6 +43,9 @@ class TestPrismClassifier(unittest.TestCase):
         print("[test_is_prediction_correct_for_multiple_inputs]:", 'Pass' if result else 'Failed')
 
     def test_is_prediction_correct_for_single_inputs(self):
+        '''
+            Test if the Prism classifier is predicting correctly for single inputs (1D array)
+        '''
         d = Dataset('teste.csv')
         p = Prism()
         p.fit(d)
