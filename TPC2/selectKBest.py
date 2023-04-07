9f06148ea4f41bf5a1524de2afd4ca08e240acc0
Hugand <hugoasgomes@hotmail.com>
2023-04-07 12:24:45 +0100

    Add SelectKBest documentation

--- TPC2/selectKBest.py
+++ TPC2/selectKBest.py
@@ -7,9 +7,7 @@ from f_classif import f_classif
 from f_regression import f_regression
 
 class SelectKBest:
-
     def __init__(self, k: int, score_func: Callable = f_classif):
-
         if k <= 0:
             raise ValueError("k isn't positive")
 
@@ -18,19 +16,44 @@ class SelectKBest:
         self.p = None
 
     def fit(self, dataset: Dataset) -> 'SelectKBest':
+        """
+        Fit the transformer with a defined scoring function
+        -------
+        dataset: Dataset
+            The dataset used to fit the transformer
+        """
         _, self.p = self.score_func(dataset)
 
     def transform(self, dataset: Dataset) -> Dataset:
+        """
+        Select the best K features of the given dataset
+        -------
+        dataset: Dataset
+            The dataset being transformed
+        -------
+        transformed_data: Dataset
+            The transformed dataset with the K best features
+        """
         indexsK = self.p.argsort()[:self.k]
         features = np.array(dataset.feature_names)[indexsK]
         
-        transformed_Data = Dataset(X=dataset.get_feature(features), y=dataset.y, features=features, label=dataset.label)
+        transformed_data = Dataset(X=dataset.get_feature(features), y=dataset.y, features=features, label=dataset.label)
 
-        return transformed_Data
+        return transformed_data
 
     def fit_transform(self, dataset: Dataset) -> Dataset:
+        """
+        Fit to the given dataset and transform it
+        -------
+        dataset: Dataset
+            The dataset being transformed
+        -------
+        transformed_data: Dataset
+            The transformed dataset with the K best features
+        """
         self.fit(dataset)
-        return self.transform(dataset)
+        transformed_data = self.transform(dataset)
+        return transformed_data
     
 if __name__ == '__main__':
     from dataset import Dataset
